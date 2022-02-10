ae9dced28fd139c315ea5f1a40d37fa8e0da9a6b
AquaApple <yassershehab9@gmail.com>
2022-02-10 12:18:36 +0200

    Added Responsive NavBar & Header Img

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
+  {path:'',component:HomeComponent},
   {path:'**',component:PageNotFoundComponent},
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HeaderComponentComponent } from './shared/header-component/header-component.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     PageNotFoundComponent,
-    NavBarComponent
+    NavBarComponent,
+    HeaderComponentComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
